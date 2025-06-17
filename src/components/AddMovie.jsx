4f74f82e804d69d65041fa75fa5ac4cfa9f091d3
Chico-WebDev <Chicowebdev@gmail.com>
2025-06-17 17:42:58 +0200

    placeholder name  url video

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -62,7 +62,7 @@ function AddMovie({ onAdd }) {
   />
     <input
     type="text"
-    placeholder="URL de l'affiche"
+    placeholder="URL de la vidéo"
     className="w-full px-4 py-2 rounded-lg bg-white/20 backdrop-blur placeholder-white text-white focus:outline-none focus:ring-2 focus:ring-amber-400"
     value={youtubeUrl}
     onChange={(e) => setYoutubeUrl(e.target.value)}
