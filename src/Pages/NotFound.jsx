6e5dfcb421844b359216a54d165f1f88ebbb089c
Chico-WebDev <Chicowebdev@gmail.com>
2025-07-07 15:29:43 +0200

    'update-notFound'

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function NotFound () {
   return (
-    <div className='text-red-700 text-4xl text-center pt-20'>NotFound 404. Revenez à l' acceuil</div>
+    <div className='text-red-700 text-4xl text-center pt-20'>NotFound 404. Ooups Revenez à l' acceuil</div>
   )
 }
 
